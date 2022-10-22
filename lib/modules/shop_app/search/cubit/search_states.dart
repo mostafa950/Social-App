8ac463dd0518751d729e2ee9d1d91964cde7b9e0
mostafa950 <76794091+mostafa950@users.noreply.github.com>
2022-10-22 14:28:26 +0200

    Delete lib/modules/shop_app directory

--- lib/modules/shop_app/search/cubit/search_states.dart
+++
@@ -1,9 +0,0 @@
-abstract class SearchStates {}
-
-class SearchInitialStates extends SearchStates {}
-
-class SearchLoadingStates extends SearchStates {}
-
-class SearchSuccessStates extends SearchStates {}
-
-class SearchErrorStates extends SearchStates {}
