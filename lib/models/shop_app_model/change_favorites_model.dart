96da407265e27941cbc12bce9b9cb106719739be
mostafa950 <76794091+mostafa950@users.noreply.github.com>
2022-10-22 14:27:08 +0200

    Delete lib/models/shop_app_model directory

--- lib/models/shop_app_model/change_favorites_model.dart
+++
@@ -1,9 +0,0 @@
-class ChangeFavoritesModel
-{
-  bool? status ;
-  String? message ;
-  ChangeFavoritesModel.fromJson(Map<String , dynamic> json){
-    this.status = json['status'];
-    this.message = json['message'];
-  }
-}
